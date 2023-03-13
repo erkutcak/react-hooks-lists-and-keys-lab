831218236bf5135dcf3deb881f76b90abb2cab76
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-03-13 19:46:56 -0400

    lab complete

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,14 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectElements = projects.map((projects) => {
+    return <ProjectItem key={projects.id} name={projects.name} about={projects.about} technologies={projects.technologies} />
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectElements}</div>
     </div>
   );
 }
